f17fe5d7c8c95e41eb4002f0c9a59f0d698cd498
Christy Wong <cww016@ucsd.edu>
2017-10-20 15:19:22 -0700

    update resume

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ var bucketModal = document.getElementById('bucket-modal');
 var bucketBtn = document.getElementById('bucket-btn');
 var mopaModal = document.getElementById('mopa-modal');
 var mopaBtn = document.getElementById('mopa-btn');
+var roadrunnerModal = document.getElementById('roadrunner-modal');
+var roadrunnerBtn = document.getElementById('roadrunner-btn');
 var bucketSpan = document.getElementsByClassName("close")[0];
 var mopaSpan = document.getElementsByClassName("close")[1];
+var roadrunnerSpan = document.getElementsByClassName("close")[2];
 
 bucketBtn.onclick = function() {
     bucketModal.style.display = "block";
@@ -11,6 +14,10 @@ bucketBtn.onclick = function() {
 
 mopaBtn.onclick = function() {
 	mopaModal.style.display = "block";
+}
+
+roadrunnerBtn.onclick = function() {
+	roadrunnerModal.style.display = "block";
 }
 
 bucketSpan.onclick = function() {
@@ -21,10 +28,16 @@ mopaSpan.onclick = function() {
 	mopaModal.style.display = "none";
 }
 
+roadrunnerSpan.onclick = function() {
+	roadrunnerModal.style.display = "none";
+}
+
 window.onclick = function(event) {
     if (event.target == bucketModal) {
         bucketModal.style.display = "none";
     } else if (event.target == mopaModal) {
     	mopaModal.style.display = "none";
+    } else if (event.target == roadrunnerModal) {
+    	roadrunnerModal.style.display = "none;"
     }
 }
