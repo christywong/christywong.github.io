badbe79666ad3f6b5e6827549dc84f34d4ee1c3b
Christy Wong <cww016@ucsd.edu>
2018-01-16 01:12:03 -0800

    added lemongrass modal

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 var bucketModal = document.getElementById('bucket-modal');
 var bucketBtn = document.getElementById('bucket-btn');
+
 var mopaModal = document.getElementById('mopa-modal');
 var mopaBtn = document.getElementById('mopa-btn');
+
 var roadrunnerModal = document.getElementById('roadrunner-modal');
 var roadrunnerBtn = document.getElementById('roadrunner-btn');
+
+var lemongrassModal = document.getElementById('lemongrass-modal');
+var lemongrassBtn = document.getElementById('lemongrass-btn');
+
 var roadrunnerSpan = document.getElementsByClassName("close")[0];
 var bucketSpan = document.getElementsByClassName("close")[1];
 var mopaSpan = document.getElementsByClassName("close")[2];
+var lemongrassSpan = document.getElementsByClassName("close")[3];
 
 bucketBtn.onclick = function() {
     bucketModal.style.display = "block";
@@ -18,6 +25,10 @@ mopaBtn.onclick = function() {
 
 roadrunnerBtn.onclick = function() {
 	roadrunnerModal.style.display = "block";
+}
+
+lemongrassBtn.onclick = function() {
+    lemongrassModal.style.display = "block";
 }
 
 bucketSpan.onclick = function() {
@@ -32,6 +43,10 @@ roadrunnerSpan.onclick = function() {
 	roadrunnerModal.style.display = "none";
 }
 
+lemongrassSpan.onclick = function() {
+    lemongrassModal.style.display = "none";
+}
+
 // if user clicks outside of modal
 window.onclick = function(event) {
     if (event.target == roadrunnerModal) {
@@ -41,5 +56,7 @@ window.onclick = function(event) {
         bucketModal.style.display = "none";
     } else if (event.target == mopaModal) {
     	mopaModal.style.display = "none";
+    } else if (event.target == lemongrassModal) {
+        lemongrassModal.style.display = "none";
     }
 }
